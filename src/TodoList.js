edc88bc333f3bf91809736e10a9115e28e0d1970
chuhx <chuhx@yonyou.com>
2019-11-18 10:24:35 +0800

    项目中使用antd 的List组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import {Input, Button} from 'antd'
+import {Input, Button,List} from 'antd'
+
+const data = [
+    '早会',
+    '阿达',
+    '那斯蒂芬'
+]
 
 export default class TodoList extends Component {
     render() {
@@ -9,14 +15,26 @@ export default class TodoList extends Component {
                 <div>
                     <Input 
                         placeholder="write"
-                        style={{width: '250px'}}
+                        style={{width: '400px', marginRight:'10px'}}
                     />
                     <Button
                         type='primary'
                     >
                         按钮
                     </Button>
-
+                </div>
+                <div style={{marginTop:"10px", width:"500px"}}>
+                    <List
+                        header={<div>Header</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                {item}
+                            </List.Item>
+                        )}
+                    />
                 </div>
             </div>
         )
