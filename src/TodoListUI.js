52f110d06e00c6fda5453541d678048747a87c67
chuhx <chuhx@yonyou.com>
2019-11-18 19:45:20 +0800

    把UI 组件修改为无状态 组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,41 +1,42 @@
-import React, { Component } from 'react'
+ // 无状态组件还是需要引入 React  因为 要使用 JSX
+import React from 'react' 
 import 'antd/dist/antd.css'
 import {Input, Button,List} from 'antd'
 
-export default class TodoListUI extends Component {
-    render() {
-        return (
-            <div style={{margin: '10px'}}>
-                <div>
-                    <Input 
-                        placeholder={this.props.placeHolder}
-                        style={{width: '400px', marginRight:'10px'}}
-                        value={this.props.inputValue}
-                        onChange={this.props.changeInputValue}
-                    />
-                    <Button
-                        type='primary'
-                        onClick={this.props.clickBtn}
-                    >
-                        按钮
-                    </Button>
-                </div>
-                <div style={{marginTop:"10px", width:"500px"}}>
-                    <List
-                        header={<div>我就是列表的头部</div>}
-                        footer={<div>Footer</div>}
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={(item, index) => (
-                            <List.Item
-                                onClick={()=>{this.props.delItem(index)}}
-                            >
-                                {item}
-                            </List.Item>
-                        )}
-                    />
-                </div>
+const TodoListUI = (props) => {
+    return (
+        <div style={{margin: '10px'}}>
+            <div>
+                <Input 
+                    placeholder={props.placeHolder}
+                    style={{width: '400px', marginRight:'10px'}}
+                    value={props.inputValue}
+                    onChange={props.changeInputValue}
+                />
+                <Button
+                    type='primary'
+                    onClick={props.clickBtn}
+                >
+                    按钮
+                </Button>
             </div>
-        )
-    }
+            <div style={{marginTop:"10px", width:"500px"}}>
+                <List
+                    header={<div>我就是列表的头部</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={props.list}
+                    renderItem={(item, index) => (
+                        <List.Item
+                            onClick={()=>{props.delItem(index)}}
+                        >
+                            {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        </div>
+    )
 }
+export default TodoListUI
+
